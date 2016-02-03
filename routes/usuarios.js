1aa388349b5502b20ad0380936dae6976acaf613
Luis Miguel Saldarriaga <luis.salda.1995@gmail.com.co>
2016-02-03 10:50:15 -0500

    Listar proveedores

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -17,16 +17,12 @@ router.use(methodOverride(function(req, res){
 }))
 
 router.route('/')
-    //GET all blobs
     .get(function(req, res, next) {
         Usuario.find(function (err, usuarios) {
               if (err) {
                   return console.error(err);
               } else {
-
                   res.json(usuarios);
-                    
-
               }     
         });
     })
@@ -52,36 +48,16 @@ router.route('/')
         });
     });
 
-// route middleware to validate :id
-router.param('id', function(req, res, next, id) {
-    //console.log('validating ' + id + ' exists');
-    //find the ID in the Database
-    mongoose.model('Blob').findById(id, function (err, blob) {
-        //if it isn't found, we are going to repond with 404
-        if (err) {
-            console.log(id + ' was not found');
-            res.status(404)
-            var err = new Error('Not Found');
-            err.status = 404;
-            res.format({
-                html: function(){
-                    next(err);
-                 },
-                json: function(){
-                       res.json({message : err.status  + ' ' + err});
-                 }
-            });
-        //if it is found we continue on
-        } else {
-            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
-            //console.log(blob);
-            // once validation is done save the new item in the req
-            req.id = id;
-            // go to the next thing
-            next(); 
-        } 
-    });
-});
+router.route('/:servicio')
+	.get(function(req, res){
+		Usuario.find({servicio: req.params.servicio}, function(err, usuarios){
+              if (err) {
+                  return console.error(err);
+              } else {
+                  res.json(usuarios);
+              }  			
+		});
+	});
 
 router.route('/:id')
   .get(function(req, res) {
