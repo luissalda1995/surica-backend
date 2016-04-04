bee9ccf03224f9f66766399e0449284added21e4
Luis Miguel Saldarriaga <luis.saldarriaga@ceiba.com.co>
2016-04-03 19:25:24 -0500

    Access control

--- config/express.js
+++ config/express.js
@@ -35,6 +35,12 @@ module.exports = function(){
 	app.use(passport.initialize());
 	app.use(passport.session());
 
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
 	app.use('/', routes);
 	app.use('/usuarios', usuarios);
 	app.use('/servicios', servicios);
